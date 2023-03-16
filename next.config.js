590fce5c20b82b4e686a490786517a01a3525dba
Matt Venables <matt@venabl.es>
2023-03-16 16:56:33 -0400

    Bump to next 13.2

--- next.config.js
+++ next.config.js
@@ -2,13 +2,6 @@
 const nextConfig = {
   experimental: {
     appDir: true
-  },
-  eslint: {
-    /**
-     * Check the app directory for eslint
-     * https://github.com/vercel/next.js/pull/44426
-     */
-    dirs: ["pages", "app", "components", "lib", "src"]
   }
 }
 
