f4be2697babe5b96fc27c307f7e81968dcf377e7
Matt Venables <matt@venabl.es>
2023-06-01 21:35:33 -0400

    Add secure headers to all responses

--- next.config.js
+++ next.config.js
@@ -1,9 +1,15 @@
+const { createSecureHeaders } = require("next-secure-headers")
+
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   experimental: {
     appDir: true,
     mdxRs: true
-  }
+  },
+  async headers() {
+    return [{ source: "/(.*)", headers: createSecureHeaders() }]
+  },
+  poweredByHeader: false
 }
 
 const withMDX = require("@next/mdx")()
