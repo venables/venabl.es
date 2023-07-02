686ad3712bb14c2fcabe14b4d45540648f42f7b4
Matt Venables <matt@venabl.es>
2023-07-02 11:35:27 -0400

    Clean up next.config.js

--- next.config.js
+++ next.config.js
@@ -2,14 +2,27 @@ const { createSecureHeaders } = require("next-secure-headers")
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  /**
+   * Toggle experimental features
+   */
   experimental: {
-    appDir: true,
     mdxRs: true
   },
+  /**
+   * Disable the `x-powered-by: nextjs` header
+   */
+  poweredByHeader: false,
+  /**
+   * Custom headers
+   */
   async headers() {
-    return [{ source: "/(.*)", headers: createSecureHeaders() }]
-  },
-  poweredByHeader: false
+    return [
+      /**
+       * Add security headers to all routes
+       */
+      { source: "/(.*)", headers: createSecureHeaders() }
+    ]
+  }
 }
 
 const withMDX = require("@next/mdx")()
