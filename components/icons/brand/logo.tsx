826723185f85dd6ae9259950b71b35032fd9202f
Matt Venables <matt@venabl.es>
2024-01-09 17:54:13 -0500

    Remove unnecessary components

--- components/icons/brand/logo.tsx
+++
@@ -1,8 +0,0 @@
-import { PowerIcon } from "lucide-react"
-import { type LucideProps } from "lucide-react"
-
-type LogoProps = LucideProps
-
-export function Logo(props: LogoProps) {
-  return <PowerIcon strokeWidth={3} {...props} />
-}
