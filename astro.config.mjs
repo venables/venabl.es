3ed11552bb604918f510588dfec0b5e2869907e9
Matt Venables <matt@venabl.es>
2024-12-30 20:52:45 -0500

    Use output: server by default

--- astro.config.mjs
+++ astro.config.mjs
@@ -8,6 +8,7 @@ import { defineConfig } from "astro/config"
 // https://astro.build/config
 export default defineConfig({
   site: "https://venabl.es",
+  output: "server",
   adapter: vercel(),
   integrations: [
     tailwind({
