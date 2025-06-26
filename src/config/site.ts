0a7f5a1cbc56fa5c9a02ec235d9afd7976ca199f
Matt Venables <matt@venabl.es>
2025-06-26 06:53:54 -0400

    Add basic astro0based shadcn button

---
+++ src/config/site.ts
@@ -0,0 +1,22 @@
+export const siteConfig = {
+  name: "Matt Venables",
+  shortName: "Venables",
+
+  author: {
+    email: "matt@venabl.es",
+    link: "https://venabl.es",
+    name: "Matt Venables",
+    twitter: "@mattyven",
+  },
+
+  description: "i build successful decentralized products.",
+  handles: {
+    twitter: "@mattyven",
+  },
+
+  links: {
+    github: "https://github.com/venables",
+    linkedin: "https://www.linkedin.com/in/mattvenables",
+    twitter: "https://twitter.com/mattyven",
+  },
+};
